fd170a756a7a2cb9e459dfeabe4cda680c6cd7f0
Joel Hernandez <joel24hernan@gmail.com>
2019-06-05 12:41:45 -0500

    Added button to html. added objects to app.js.

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+var questionOne = {
+    question: "Who won the 1999 NBA championship?",
+    answersOne: "['bulls','knicks','spurs','lakers']",
+    correctAnswerOne: "spurs"
+}
+
+var questionTwo = {
+    question: "How many superbowls do the patriots have?",
+    answersTwo: "['8','6','15','3']",
+    correctAnswerTwo: "6"
+}
+
+var questionThree = {
+    question: "How many superbowls do the patriots have?",
+    answersTwo: "['8','6','15','3']",
+    correctAnswerTwo: "6"
+}
